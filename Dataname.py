d4ac608229ed3743ba2374b48906c7aaa1f1e28f
YoKenSenkai <thainakdee06@gmail.com>
2023-09-01 17:16:34 +0700

    commit 3

--- Dataname.py
+++ Dataname.py
@@ -14,7 +14,7 @@ dataname = Blueprint('dataname',__name__)
 @dataname.route('/showdata')
 def showdata():
     cursor = connection.cursor()
-    sql = ("SELECT * FROM customer")
+    sql = ("SELECT customer.id, item_list.id_item, item_list.brand, item_list.serial_nb, customer.fname, customer.department FROM item_list INNER JOIN customer ON item_list.fname = customer.fname")
     cursor.execute(sql)
     row = cursor.fetchall()
     print(row)
