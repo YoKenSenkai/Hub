4d1ac800a2c2ba6954dc5a9db5f72a1fb9e13fe5
YoKenSenkai <thainakdee06@gmail.com>
2023-09-13 16:37:25 +0700

    commit 5

--- Dataname.py
+++ Dataname.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request
 import pymysql
 from Config import *
 
@@ -15,14 +15,22 @@ dataname = Blueprint('dataname',__name__)
 def showdata():
     cursor = connection.cursor()
     sql = ("SELECT customer.id, item_list.id_item, item_list.brand, item_list.serial_nb, customer.fname, customer.lname, dm.department FROM item_list INNER JOIN customer ON item_list.fname = customer.fname INNER JOIN dm ON customer.department = dm.department")
+    sql2 = ("SELECT * FROM dm")
     cursor.execute(sql)
+    
     row = cursor.fetchall()
-    return render_template("tablename.html",headername = "รายชื่อ" ,datas = row)
 
-@dataname.route('/editdata')
+    cursor.execute(sql2)
+    department = cursor.fetchall()
+    return render_template("tablename.html",headername = "รายชื่อ" ,datas = row, departments = department)
+
+@dataname.route('/editdata',methods=["POST"])
 def editdata():
     cursor = connection.cursor()
-    sql = ("SELECT * FROM dm")
-    cursor.execute(sql)
-    row = cursor.fetchall()
-    return render_template("tablename.html", departments = row)
+    if request.method == "POST":
+        fname = request.form['fname']
+        lname = request.form['lname']
+        dm = request.form['department'] 
+    sql = ("UPDATE customer SET fname = %s ,lname = %s ,department = %s WHERE id = %s")
+    cursor.excute(sql,('fname = name','lname','id'))
+    return render_template("tablename.html")
